065753fd48f4e55d899f0ed28599148740470ff7
penchochris <garabchris@gmail.com>
2018-08-30 00:54:25 +0200

    #01# Show data as a component from mocked database and use Dragula with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DragulaService } from "ng2-dragula";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-dnd';
+  
+  items = [];
+
+  ngOnInit() {
+    const url = 'http://www.mocky.io/v2/59f7760a2f0000ab1d55864e';
+
+    fetch(url)
+      .then(blob => blob.json())
+      .then(data => this.items = data.form)
+
+  }
+
+  public constructor(private dragulaService: DragulaService) {
+    dragulaService.createGroup("draggable", {
+      revertOnSpill: true
+    });
+  }
 }
