906f4ae40860179c5c60e6a9f4f3dafd7700e8d7
penchochris <garabchris@gmail.com>
2018-08-30 13:36:49 +0200

    #03# Adding some styles like fixed header, scrollable list and showing and formatting the header area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,41 @@ import { DragulaService } from "ng2-dragula";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
   
   items = [];
+  formId = '';
+  lastChangedDate = '';
+  lastChangedBy = '';
+
+  getFormattedDate(date) {
+    const dd = date.getDate();
+    const mm = date.getMonth()+1;
+    const yyyy = date.getFullYear();
+    const hh = date.getHours();
+    const mn = date.getMinutes();
+    const str = `${dd}/${mm}/${yyyy} ${hh}:${mn}`;
+
+    return str;
+  }
 
   ngOnInit() {
     const url = 'http://www.mocky.io/v2/59f7760a2f0000ab1d55864e';
 
     fetch(url)
       .then(blob => blob.json())
-      .then(data => this.items = data.form)
+      .then(data => {
+
+        const date = new Date(data.lastChangedDate);
+
+        this.items = data.form;
+        this.formId = data.formId;
+        this.lastChangedBy = data.lastChangedBy;
+        this.lastChangedDate = `${this.getFormattedDate(date)}`;
+      })
   }
 
   public constructor(private dragulaService: DragulaService) {
